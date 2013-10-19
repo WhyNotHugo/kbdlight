4faea5242ef7f475bdf2c356bc3aa33c417c995c
John Tsiombikas <nuclear@mutantstargoat.com>
2013-10-20 01:42:10 +0300

    added set/get commands, added error checking

--- kbdlight.c
+++ kbdlight.c
@@ -2,22 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BLPATH		"/sys/class/leds/smc::kbd_backlight"
+#define BLVALFILE	BLPATH "/brightness"
+#define BLMAXFILE	BLPATH "/max_brightness"
+
 void usage() {
     printf("Usage: kbdlight [up|down|off|max]\n");
     exit(0);
 }
 
 int main(int argc, char* argv[]) {
-  int current, next;
+  int current, next, maxval;
   FILE *file;
 
-  if (argc != 2) {
+  if (argc < 2) {
     usage();
   }
 
-  file = fopen("/sys/class/leds/smc::kbd_backlight/brightness", "r");
+  if(!(file = fopen(BLVALFILE, "r"))) {
+    perror("failed to open the keyboard backlight file: " BLVALFILE);
+    return 1;
+  }
   fscanf(file, "%d", &current);
   fclose(file);
+
+  if((file = fopen(BLMAXFILE, "r"))) {
+    fscanf(file, "%d", &maxval);
+    fclose(file);
+  } else {
+    maxval = 255;
+  }
 
   if (!strcmp(argv[1], "up"))
     next = current + 16;
@@ -27,11 +41,23 @@ int main(int argc, char* argv[]) {
     next = 0;
   else if (!strcmp(argv[1], "max"))
     next = 255;
-  else
+  else if (!strcmp(argv[1], "set")) {
+    char *endp;
+    if(!argv[2] || (next = strtol(argv[2], &endp, 10), endp == argv[2])) {
+      fprintf(stderr, "set must be followed by a number from 0 to %d\n", maxval);
+      return 1;
+    }
+  } else if(!strcmp(argv[1], "get")) {
+    printf("%d\n", current);
+    return 0;
+  } else
     usage();
 
-  file = fopen("/sys/class/leds/smc::kbd_backlight/brightness", "w");
-  fprintf(file, "%d", next);
-  fclose(file);
+  if((file = fopen(BLVALFILE, "w"))) {
+    fprintf(file, "%d", next);
+    fclose(file);
+  } else {
+    perror("failed to write to the keyboard backlight file: " BLVALFILE);
+  }
   return 0;
 }
